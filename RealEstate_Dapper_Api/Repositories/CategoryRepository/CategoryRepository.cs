816f37b6c44757c96a50eb65b7fcd8d0a38f5bcc
tuncmert <merttunc20@gmail.com>
2023-09-15 15:49:32 +0300

    create context

---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            string query = "Select * From Category";
+            using(var connection=_context.CreateConnection()) 
+            { 
+                var values = await connection.QueryAsync<ResultCategoryDto>(query);       
+                return values.ToList(); 
+            }
+        }
+    }
+}
